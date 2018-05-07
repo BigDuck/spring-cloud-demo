23675de1aa594b1e5df3365fbf4009e775c22866
wupj <wpjlovehome@gmail.com>
2018-05-07 23:12:21 +0800

    spring -boot admin

---
+++ parent/actuator-admin/src/main/java/com/wupj/cloud/SpringBootAdminApplication.java
@@ -0,0 +1,18 @@
+package com.wupj.cloud;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author WPJ587
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
